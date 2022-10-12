58ec654debe4ed3d658f680a16fa4eab1ab705e7
陆挺 <474628778@qq.com>
2022-10-12 21:57:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o7joX6pQGZxilp4kuPTqFWqMzZiI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'bolLo65yfxLkDvyFLx_N2pRfp7Q_2LO9Skq7Zz8tB_Y',
+      useTemplateId: '20jd2MkseivqTWROuijM3TvXm2WuWNdaVtdPixESasI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '09-29',
       festivals: [
